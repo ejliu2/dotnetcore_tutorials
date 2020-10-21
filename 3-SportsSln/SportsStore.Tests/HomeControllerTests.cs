649ada0cae9d80bc1d1a1929aacb3961d81642d1
Eric L <ejl2@sfu.ca>
2020-10-21 15:23:01 -0700

    ch 8 - add basic navigation component, fixed upload from subfolder

--- 3-SportsSln/SportsStore.Tests/HomeControllerTests.cs
+++ 3-SportsSln/SportsStore.Tests/HomeControllerTests.cs
@@ -22,7 +22,7 @@ namespace SportsStore.Tests {
 
             // Act
             // IEnumerable<Product> result =(controller.Index() as ViewResult).ViewData.Model as IEnumerable<Product>;
-            ProductsListViewModel result = controller.Index().ViewData.Model as ProductsListViewModel;
+            ProductsListViewModel result = controller.Index(null).ViewData.Model as ProductsListViewModel;
 
             // Assert
             Product[] prodArray = result.Products.ToArray();
@@ -47,13 +47,65 @@ namespace SportsStore.Tests {
 
             // Act
             // IEnumerable<Product> result = (controller.Index(2) as ViewResult).ViewData.Model as IEnumerable<Product>;
-            ProductsListViewModel result = controller.Index(2).ViewData.Model as ProductsListViewModel;
+            ProductsListViewModel result = controller.Index(null, 2).ViewData.Model as ProductsListViewModel;
 
             // Assert
             Product[] prodArray = result.Products.ToArray();
             Assert.True(prodArray.Length == 2);
             Assert.Equal("P4", prodArray[0].Name);
             Assert.Equal("P5", prodArray[1].Name);
+        }
+
+        [Fact]
+        public void Can_Send_Pagination_View_Model() {
+            // Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            }).AsQueryable<Product>());
+            HomeController controller = new HomeController(mock.Object) { PageSize = 3 };
+            controller.PageSize = 3;
+
+            // Act
+            // IEnumerable<Product> result = (controller.Index(2) as ViewResult).ViewData.Model as IEnumerable<Product>;
+            ProductsListViewModel result = controller.Index(null, 2).ViewData.Model as ProductsListViewModel;
+
+            // Assert
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.Equal(2, pageInfo.CurrentPage);
+            Assert.Equal(3, pageInfo.ItemsPerPage);
+            Assert.Equal(5, pageInfo.TotalItems);
+            Assert.Equal(2, pageInfo.TotalPages);
+        }
+
+        [Fact]
+        public void Can_Filter_Products() {
+            // Arrange
+            // create the mock repository
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(mock => m.Products).Returns((new ProductControllerTests[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+                new Product {ProductID = 1, Name = "P2", Category = "Cat2"},
+                new Product {ProductID = 1, Name = "P3", Category = "Cat1"},
+                new Product {ProductID = 1, Name = "P4", Category = "Cat2"},
+                new Product {ProductID = 1, Name = "P5", Category = "Cat3"}
+            }).AsQueryable<Product>());
+
+            // Arrange
+            HomeController controller = new HomeController(mock.Object);
+            controller.PageSize = 3;
+
+            // Action
+            Product[] results = (controller.Index("Cat2", 1).ViewData.Model as ProductsListViewModel).Products.ToArray();
+
+            // Assert
+            Assert.Equal(2, results.Length);
+            Assert.True(results[0].Name == "P2" && results[0].Category == "Cat2");
+            Assert.True(results[1].Name == "P4" && results[1].Category == "Cat2");
         }
     }
 }
